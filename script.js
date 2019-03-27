338b9fc2e97ccd63218cdcd5b26cd4acd0b54584
Essau Tesfay <essautesfay.te@gmail.com>
2019-03-27 21:16:08 +0100

    defining variables in JS

---
+++ script.js
@@ -0,0 +1,22 @@
+var firstName = 'John';
+console.log(firstName);
+// We use the camelcase notation here because its best practice not a rule.
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+// update btw lol
+job = 'Teacher';
+console.log(job);
+
+//var 3years = 3; is invalid and will give syntax error.
+var _3years = 3; //Will work or with a $
+// Another example is that if you put a symbol in the middle of a variabel it won't work. 
+//var john/mark = 'John and Mark';
+// A correction of this is.
+var johnMark = 'John and Mark';
+// We can't declare reserved keywords like
+//var function,delete,if = 23; Won't work.
